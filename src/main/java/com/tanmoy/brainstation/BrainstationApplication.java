178a2e019829997217ba830fdad343b7e0d37a9e
Tanmoy Tushar <46753634+tanmoy069@users.noreply.github.com>
2021-07-05 22:34:50 +0600

    Project initialize

---
+++ src/main/java/com/tanmoy/brainstation/BrainstationApplication.java
@@ -0,0 +1,13 @@
+package com.tanmoy.brainstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmoy/brainstation/BrainstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmoy.brainstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
