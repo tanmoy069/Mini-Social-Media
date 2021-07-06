e806453dd75abf84365cf6126d144ad254a65cf6
tanmoy <pdt.tanmoy@gmail.com>
2021-07-06 20:02:36 +0600

    Move /home api from MiniSocialMediaConftroller to StatusController

--- src/main/java/com/tanmoy/brainstation/controller/MiniSocialMediaController.java
+++ src/main/java/com/tanmoy/brainstation/controller/MiniSocialMediaController.java
@@ -1,8 +1,6 @@
 package com.tanmoy.brainstation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,45 +12,34 @@ import com.tanmoy.brainstation.service.UserService;
 
 @Controller
 public class MiniSocialMediaController {
-	
+
 	UserService userService;
-	
+
 	@Autowired
 	public MiniSocialMediaController(UserService userService) {
 		this.userService = userService;
 	}
-	
-	@GetMapping(value ="/login")
+
+	@GetMapping(value = "/login")
 	public String login() {
 		return "Login";
 	}
-	
-	@GetMapping(value ="/registration")
+
+	@GetMapping(value = "/registration")
 	public String getRegistrationFrom() {
 		return "Registration";
 	}
-	
-	@PostMapping(value ="/registration")
+
+	@PostMapping(value = "/registration")
 	public String submitRegistration(Model model, @ModelAttribute("userRegiForm") User user) {
 		String bycrptPass = userService.getBycrptPassword(user.getPassword());
 		user.setPassword(bycrptPass);
 		System.out.println(user.toString());
 		model.addAttribute("regiRequest", true);
 		boolean isSave = userService.save(user);
-		if(isSave) model.addAttribute("successful", true);
+		if (isSave) model.addAttribute("successful", true);
 		else model.addAttribute("unsuccessful", true);
 		return "Registration";
 	}
-	
-	@GetMapping(value="/home")
-	public String getHome(Model model) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userService.findById(auth.getName());
-		model.addAttribute("userDetails", user);
-		model.addAttribute("userId", user.getUserId());
-		return "Home";
-	}
-	
-	
 
 }
