d5bf9db94184168dcbda35c7e319d984d71a9499
tanmoy <pdt.tanmoy@gmail.com>
2021-07-07 13:57:55 +0600

    Java doc added to AbstractService class

--- src/main/java/com/tanmoy/brainstation/service/AbstractService.java
+++ src/main/java/com/tanmoy/brainstation/service/AbstractService.java
@@ -5,9 +5,23 @@ import java.util.logging.Logger;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * AbstractService class can be used globally for all the service class of
+ * domain classes.
+ * 
+ * @author tanmoy.tushar
+ * @since 2021-07-06
+ * @param <T> pass domain class here
+ */
 public abstract class AbstractService<T> {
 	protected final static Logger LOGGER = Logger.getLogger(AbstractService.class.getName());
 
+	/**
+	 * Get integer from string value
+	 * 
+	 * @param str
+	 * @return int value
+	 */
 	public int getInt(String str) {
 		try {
 			return Integer.parseInt(str);
@@ -15,19 +29,55 @@ public abstract class AbstractService<T> {
 			return 0;
 		}
 	}
-	
+
+	/**
+	 * Get bycrpted password from any string
+	 * 
+	 * @param password
+	 * @return bycrpt password
+	 */
 	public String getBycrptPassword(String password) {
 		BCryptPasswordEncoder bcryptPassword = new BCryptPasswordEncoder(12);
 		return bcryptPassword.encode(password);
 	}
-	
+
+	/**
+	 * Get T object by id, when type of id is integer, if you have another type of
+	 * id then overload the method
+	 * 
+	 * @param id
+	 * @return T object
+	 */
 	public abstract T findById(int id);
 
+	/**
+	 * Get list of all rows of T table.
+	 * 
+	 * @return all rows of T
+	 */
 	public abstract List<T> findAll();
 
+	/**
+	 * To save T object
+	 * 
+	 * @param obj
+	 * @return true if successfully saved, otherwise false;
+	 */
 	public abstract boolean save(T obj);
 
+	/**
+	 * To update T object
+	 * 
+	 * @param obj
+	 * @return true if successfully updated, otherwise false;
+	 */
 	public abstract boolean update(T obj);
 
+	/**
+	 * To delete T object
+	 * 
+	 * @param id
+	 * @return true if successfully deleted, otherwise false;
+	 */
 	public abstract boolean deleteById(int id);
 }
